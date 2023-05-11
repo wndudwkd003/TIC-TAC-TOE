106fd6e38602928fdc45ace8f2f64fd44305e3bc
wndudwkd003 <ymail3@naver.com>
2023-05-11 22:53:04 +0900

    확률 90 수정

--- app/src/main/java/com/zynar/tic_tac_toe/helpers/AIHelper.java
+++ app/src/main/java/com/zynar/tic_tac_toe/helpers/AIHelper.java
@@ -17,7 +17,7 @@ public class AIHelper {
         int randomNum = random.nextInt(100);
         List<List<Integer>> emptyList = new ArrayList<>();
 
-        if (randomNum >= 85) {
+        if (randomNum >= 90) {
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     if (board[i][j] == 0) {

0d56aaf425a6af788208812c1702be99ad84fdbb
hong0002 <hong_0002@naver.com>
2023-05-11 22:30:19 +0900

    인공지능 난이도 낮추기

--- app/src/main/java/com/zynar/tic_tac_toe/helpers/AIHelper.java
+++ app/src/main/java/com/zynar/tic_tac_toe/helpers/AIHelper.java
@@ -1,4 +1,8 @@
 package com.zynar.tic_tac_toe.helpers;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class AIHelper {
     private static final int MIN_NUMBER = -10000;
@@ -7,20 +11,69 @@ public class AIHelper {
     public int[] getNextMove(int[][] board) {
         int maxEval = MIN_NUMBER;
         int[] move = new int[2];
+
+        Random random = new Random();
+        random.setSeed(System.currentTimeMillis());
+        int randomNum = random.nextInt(100);
+        List<List<Integer>> emptyList = new ArrayList<>();
+
+        boolean flag = false;
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board[i][j] == 0) {
-                    board[i][j] = 2;
-                    int eval = minimax(board, 0, false);
-                    board[i][j] = 0;
-                    if (eval > maxEval) {
-                        maxEval = eval;
-                        move[0] = i;
-                        move[1] = j;
+                    flag = true;
+                }
+            }
+        }
+        if (flag == true) {
+            if (randomNum >= 60) {
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (board[i][j] == 0) {
+                            emptyList.add(Arrays.asList(i, j));
+                        }
+                    }
+                }
+                int index = random.nextInt(emptyList.size()-1);
+                move[0] = emptyList.get(index).get(0);
+                move[1] = emptyList.get(index).get(1);
+                emptyList.clear();
+            }
+            else {
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (board[i][j] == 0) {
+                            board[i][j] = 2;
+                            int eval = minimax(board, 0, false);
+                            board[i][j] = 0;
+                            if (eval > maxEval) {
+                                maxEval = eval;
+                                move[0] = i;
+                                move[1] = j;
+                            }
+                        }
                     }
                 }
             }
         }
+        else {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (board[i][j] == 0) {
+                        board[i][j] = 2;
+                        int eval = minimax(board, 0, false);
+                        board[i][j] = 0;
+                        if (eval > maxEval) {
+                            maxEval = eval;
+                            move[0] = i;
+                            move[1] = j;
+                        }
+                    }
+                }
+            }
+        }
+        flag = false;
         return move;
     }
     public int minimax(int[][] board, int depth, boolean isMax) {
